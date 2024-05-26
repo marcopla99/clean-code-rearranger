d8f4f37f6d2e8454825549109d8bfcb235632619
GitHub Action <action@github.com>
2024-05-26 20:15:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/marcopla99/cleancoderearranger/MyBundle.kt
+++ src/main/kotlin/com/github/marcopla99/cleancoderearranger/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.marcopla99.cleancoderearranger
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/marcopla99/cleancoderearranger/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/marcopla99/cleancoderearranger/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.marcopla99.cleancoderearranger.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
