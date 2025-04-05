2a14e3d5e8432c2898f8beb31363a8884240f58f
Marco <plasmati@d-tt.nl>
2025-04-05 11:01:58 +0200

    Add FunctionsRearrangerAction

---
+++ src/main/kotlin/com/github/marcopla99/cleancoderearranger/actions/FunctionsRearrangerAction.kt
@@ -0,0 +1,10 @@
+package com.github.marcopla99.cleancoderearranger.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class FunctionsRearrangerAction: AnAction() {
+    override fun actionPerformed(actionEvent: AnActionEvent) {
+        println("action is performed")
+    }
+}
