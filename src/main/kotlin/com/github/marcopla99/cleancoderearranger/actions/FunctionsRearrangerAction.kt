dd1c102fd79b5a3f3eb50c9f6fc2f89e92790e20
Marco <plasmati@d-tt.nl>
2025-04-05 11:35:24 +0200

    Rearrange functions from a KtFile

--- src/main/kotlin/com/github/marcopla99/cleancoderearranger/actions/FunctionsRearrangerAction.kt
+++ src/main/kotlin/com/github/marcopla99/cleancoderearranger/actions/FunctionsRearrangerAction.kt
@@ -1,10 +1,22 @@
 package com.github.marcopla99.cleancoderearranger.actions
 
+import com.github.marcopla99.cleancoderearranger.FunctionCallGraphs
+import com.github.marcopla99.cleancoderearranger.rearranger.GraphRearranger
+import com.github.marcopla99.cleancoderearranger.util.getSignature
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.LangDataKeys
+import org.jetbrains.kotlin.psi.KtFile
 
 class FunctionsRearrangerAction: AnAction() {
-    override fun actionPerformed(actionEvent: AnActionEvent) {
-        println("action is performed")
+    override fun actionPerformed(anActionEvent: AnActionEvent) {
+        val file = anActionEvent.getData(LangDataKeys.PSI_FILE) as KtFile
+        println("file: ${file.text}")
+        val functionCallGraphs = FunctionCallGraphs(file)
+        println("functionCallGraphs: $functionCallGraphs")
+        val rearrangedFunctions = functionCallGraphs.graphs.map { graph ->
+            GraphRearranger.rearrange(graph)
+        }
+        println("rearrangedFunctions: ${rearrangedFunctions.flatten().map { it.getSignature() }}")
     }
 }
