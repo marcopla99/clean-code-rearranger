08af672f9f7deee53462fe72449972d91521e05b
Marco <plasmati@d-tt.nl>
2025-04-06 10:26:47 +0200

    Reformat code after rearrangement

--- src/main/kotlin/com/github/marcopla99/cleancoderearranger/actions/FunctionsRearrangerAction.kt
+++ src/main/kotlin/com/github/marcopla99/cleancoderearranger/actions/FunctionsRearrangerAction.kt
@@ -2,41 +2,43 @@ package com.github.marcopla99.cleancoderearranger.actions
 
 import com.github.marcopla99.cleancoderearranger.FunctionCallGraphs
 import com.github.marcopla99.cleancoderearranger.rearranger.GraphRearranger
-import com.github.marcopla99.cleancoderearranger.util.getSignature
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.actionSystem.LangDataKeys
-import com.intellij.util.DocumentUtil
+import com.intellij.openapi.command.WriteCommandAction
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.codeStyle.CodeStyleManager
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.psiUtil.endOffset
+import org.jetbrains.kotlin.psi.psiUtil.startOffset
 
-class FunctionsRearrangerAction: AnAction() {
+class FunctionsRearrangerAction : AnAction() {
     override fun actionPerformed(anActionEvent: AnActionEvent) {
         val file = anActionEvent.getData(LangDataKeys.PSI_FILE) as? KtFile ?: return
-        println("file: ${file.text}")
-        val functionCallGraphs = FunctionCallGraphs(file)
-        println("functionCallGraphs: $functionCallGraphs")
-        val rearrangedFunctions = functionCallGraphs.graphs.map { graph ->
-            GraphRearranger.rearrange(graph)
-        }
-        println("rearrangedFunctions: ${rearrangedFunctions.flatten().map { it.getSignature() }}")
+        val project = anActionEvent.project ?: return
         val document = anActionEvent.getData(CommonDataKeys.EDITOR)?.document ?: return
+        val documentManager = PsiDocumentManager.getInstance(project)
+        val functionCallGraphs = FunctionCallGraphs(file)
+        val rearrangedFunctions = functionCallGraphs.graphs.map { graph -> GraphRearranger.rearrange(graph) }
         val roots = rearrangedFunctions.mapNotNull { it.firstOrNull() }
         val children = rearrangedFunctions.map { it.filterNot { function -> function in roots } }
-        DocumentUtil.writeInRunUndoTransparentAction {
-            children.flatten().forEach { it.delete() }
-            println("deleted")
+        WriteCommandAction.runWriteCommandAction(project, "Rearrange Functions", "RearrangeFunctions", {
+            children.flatten().forEach {
+                documentManager.commitDocument(document)
+                document.deleteString(it.startOffset, it.endOffset)
+            }
             roots.forEachIndexed { index, root ->
                 var offset = root.endOffset
-                println("root offset: $offset")
                 val elementsToInsertBack = children[index]
-                println("elementsToInsertBack: ${elementsToInsertBack.joinToString { it.text }}")
                 elementsToInsertBack.forEach {
+                    documentManager.commitDocument(document)
                     document.insertString(offset, it.text)
                     offset += it.textRange.endOffset
                 }
             }
-        }
+            documentManager.commitDocument(document)
+            CodeStyleManager.getInstance(project).reformat(file)
+        }, file)
     }
 }
