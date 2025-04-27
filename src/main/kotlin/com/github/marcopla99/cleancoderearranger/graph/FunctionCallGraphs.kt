b705386337febcd051e98468de026d3a19699154
Marco <plasmati@d-tt.nl>
2025-04-27 11:19:39 +0200

    Extract graph additions

--- src/main/kotlin/com/github/marcopla99/cleancoderearranger/graph/FunctionCallGraphs.kt
+++ src/main/kotlin/com/github/marcopla99/cleancoderearranger/graph/FunctionCallGraphs.kt
@@ -22,8 +22,7 @@ class FunctionCallGraphs(file: KtFile) {
             override fun visitElement(element: PsiElement) {
                 val graphKey = element.parentOfType<KtClass>() ?: file
                 if (element is KtNamedFunction) {
-                    // todo extract
-                    _graphs[graphKey] = _graphs[graphKey].orEmpty() + (element to _graphs[graphKey]?.get(element).orEmpty())
+                    addCalleeToParentInGraph(graphKey = graphKey, parent = element, callee = null)
                 }
                 if (element is KtCallExpression) {
                     // todo make sure it's part of the file?
@@ -33,14 +32,23 @@ class FunctionCallGraphs(file: KtFile) {
                         ?.references
                         ?.firstNotNullOfOrNull { it.resolve() as? KtFunction }
                         ?: element.parentOfType<KtNamedFunction>()
-                    if (callee != null) {
-                        parent?.let { _graphs[graphKey] = _graphs[graphKey].orEmpty() + (parent to _graphs[graphKey]?.get(parent).orEmpty() + callee) }
+                    if (callee != null && parent != null) {
+                        addCalleeToParentInGraph(graphKey = graphKey, parent = parent, callee = callee)
                     }
                 }
                 super.visitElement(element)
             }
         }
         visitor.visitElement(file)
+    }
+
+    private fun addCalleeToParentInGraph(
+        graphKey: KtDeclarationContainer,
+        parent: KtFunction,
+        callee: KtFunction?
+    ) {
+        val calleesInParent = _graphs[graphKey]?.get(parent).orEmpty().let { if (callee != null) it + callee else it }
+        _graphs[graphKey] = _graphs[graphKey].orEmpty() + (parent to calleesInParent)
     }
 
     override fun toString(): String = "[" + graphs.values.joinToString { graph ->
